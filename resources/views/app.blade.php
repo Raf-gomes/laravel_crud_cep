53515a2ed1320e1323b8324a38ab2b4ec06be624
Raf-gomes <Rafaeldamasceno3@gmail.com>
2025-01-31 19:25:27 -0300

    comentário no arquivo app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     <body>
+        <!-- Para completar o CRUD é necessário cria o UPDATE e o DELETE!! Não esquecer. -->
         <div class="container">
             @yield('content')
         </div>
